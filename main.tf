8d74448af1330a7c10bbf65be74dd97d9f4e2007
KamireddySaikiran <kamireddy.saikiran@thecloudside.com>
2025-09-02 21:16:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "google" {
-  project = "your-project-id"
-  region  = "us-central1"
+  project = var.project_id
+  region  = var.region
 }
 
 resource "google_container_cluster" "primary" {
   name     = "test-gke-cluster"
-  location = "us-central1"
+  location = var.region
 
   remove_default_node_pool = true
   initial_node_count       = 1
@@ -13,12 +13,17 @@ resource "google_container_cluster" "primary" {
 
 resource "google_container_node_pool" "primary_nodes" {
   name       = "test-node-pool"
-  location   = "us-central1"
+  location   = var.region
   cluster    = google_container_cluster.primary.name
   node_count = 1
 
   node_config {
     preemptible  = true
     machine_type = "e2-small"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
   }
 }
