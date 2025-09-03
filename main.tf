82c70324ee32eccc38977bb51f68082920beac13
KamireddySaikiran <kamireddy.saikiran@thecloudside.com>
2025-09-03 14:08:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,3 +15,18 @@ resource "google_container_cluster" "primary" {
     disk_size_gb = 20          # Minimal disk size
   }
 }
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "test-node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  # Add explicit dependency
+  depends_on = [google_container_cluster.primary]
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-small"
+    # ... rest of config
+  }
+}
