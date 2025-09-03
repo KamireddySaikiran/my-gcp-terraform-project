1a220ac9b27eefa57f876f139b89960adc367f4e
KamireddySaikiran <kamireddy.saikiran@thecloudside.com>
2025-09-03 13:08:20 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
 variable "project_id" {}
-variable "region" {}
+variable "region" {
+  description = "The GCP region"
+  type        = string
+  default     = "us-west1"  # or "europe-west1", "asia-southeast1"
+}
 variable "credentials_file" {}
