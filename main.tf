87dd5af67ed4d31ccfb2564bb79126009e3dce7f
KamireddySaikiran <kamireddy.saikiran@thecloudside.com>
2025-09-03 13:21:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,20 +4,8 @@ provider "google" {
   credentials = file(var.credentials_file)
 }
 
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "test-node-pool"
-  location   = var.region
-  cluster    = google_container_cluster.primary.name
-  node_count = 1
-
-  node_config {
-    preemptible  = true
-    machine_type = "e2-micro"  # ← CHANGE from e2-small to e2-micro
-    disk_size_gb = 20          # ← ADD this to reduce disk size
-    
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-  }
+resource "google_container_cluster" "primary" {
+  name     = "demo-gke"
+  location = var.region
+  initial_node_count = 1
 }
