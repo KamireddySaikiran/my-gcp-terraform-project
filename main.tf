ba58366f66f9077bc3592605ea08168592568ca8
KamireddySaikiran <kamireddy.saikiran@thecloudside.com>
2025-09-03 13:31:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,15 @@ provider "google" {
   region      = var.region
   credentials = file(var.credentials_file)
 }
-
 resource "google_container_cluster" "primary" {
-  name     = "demo-gke"
+  name     = "minimal-gke-cluster"
   location = var.region
   initial_node_count = 1
+  remove_default_node_pool = true
+  
+  # Add this to specify node configuration at cluster level
+  node_config {
+    disk_type = "pd-standard"  # Use standard disks, not SSD
+    disk_size_gb = 20          # Minimal disk size
+  }
 }
