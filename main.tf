b703aa3a77ff5832c5500f2d2aba3ed8a4d3a75a
KamireddySaikiran <kamireddy.saikiran@thecloudside.com>
2025-09-03 15:30:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,17 +3,25 @@ provider "google" {
   region      = var.region
   credentials = file(var.credentials_file)
 }
+# This should be in your main.tf
+resource "google_container_cluster" "primary" {
+  name     = "minimal-gke-cluster"
+  location = "us-west1"
+  initial_node_count = 1
+  remove_default_node_pool = true
+}
+
 resource "google_container_node_pool" "primary_nodes" {
   name       = "minimal-pool"
-  location   = var.region
+  location   = "us-west1"
   cluster    = google_container_cluster.primary.name
   node_count = 1
 
   node_config {
     preemptible  = true
-    machine_type = "e2-micro"    # Smallest instance
-    disk_type    = "pd-standard" # ← CRITICAL: Use STANDARD disks, not SSD
-    disk_size_gb = 20            # Minimal disk size
+    machine_type = "e2-micro"
+    disk_type    = "pd-standard"
+    disk_size_gb = 20
     
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
